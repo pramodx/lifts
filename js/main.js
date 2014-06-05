6426142eed9f54747ec6bad174e59c00f1e83cec
pramodx <pramodx@gmail.com>
2014-06-06 01:00:45 +1000

    completed drop downs, and lift status. Refactored code

--- js/main.js
+++ js/main.js
@@ -7,41 +7,48 @@ var Escalator = (function($, undefined){
 	var _getLiftRef = function(t){
 		return $(t).attr('data-lift');
 	}
+
+	var _move = function(lift, targetfloor){
+		//variables
+		var sourcefloorref = parseInt(_getFloor(lift),10),
+			sourcefloor = $('tr[data-floor='+sourcefloorref+']'),
+			liftsonfloor = parseInt(sourcefloor.attr('data-liftcount'),10),
+			liftref = _getLiftRef(lift),		
+			i = sourcefloorref,
+			targetfloorref = parseInt(targetfloor,10),
+			targetfloor = $('tr[data-floor='+targetfloorref+']'),
+			topStatus = $('thead th.status').eq(liftref-1);
+
+		//actions
+		$(lift).removeClass('stopped');
+		
+		sourcefloor.attr('data-liftcount', liftsonfloor-1);		
+		if (sourcefloor.attr('data-liftcount')==0){
+			sourcefloor.find('select').removeClass('show');	
+		}	
+			
+		var interval = setInterval(function() {
+			
+			$('tr[data-floor='+i+']').find('td[data-lift='+liftref+']').removeClass('moving');	
+			
+			if ( sourcefloorref > targetfloorref ) {i--; topStatus.find('p').removeClass('arrow-up arrow-down').addClass('arrow-down')}
+			else if ( sourcefloorref < targetfloorref ) {++i; topStatus.find('p').removeClass('arrow-up arrow-down').addClass('arrow-up')}
+				
+		    $('tr[data-floor='+i+']').find('td[data-lift='+liftref+']').addClass('moving');	
+		    
+		    if(i===targetfloorref) {
+		        clearInterval(interval);
+		        //alert("Code to be executed after completing loop");
+		       targetfloor.find('td[data-lift='+liftref+']').removeClass('moving').addClass('stopped');	
+		       targetfloor.attr('data-liftcount', parseInt(targetfloor.attr('data-liftcount'),10)+1);
+		        targetfloor.find('select').addClass('show');
+		        topStatus.find('p').removeClass('arrow-up arrow-down');
+		    }
+		}, 1500);
+	}
 	
-	var _moveUp = function(lift, floorref){
-		$(lift).removeClass('stopped');
-		var liftref = _getLiftRef(lift);		
-		var i = floorref;
-		var interval = setInterval(function() {
-			//$('tr[data-floor='+(i-1)+']').find('td[data-lift='+liftref+']').removeClass('moving');	
-		    $('tr[data-floor='+i+']').find('td[data-lift='+liftref+']').addClass('moving');	
-		    i++;
-		    if(i>floorref) {
-		        clearInterval(interval);
-		        //alert("Code to be executed after completing loop");
-		        $('tr[data-floor='+floorref+']').find('td[data-lift='+liftref+']').removeClass('moving').addClass('stopped');	
-		    }
-		}, 1500);
 
-
-	}
-	var _moveDown = function(lift, floorref){
-		$(lift).removeClass('stopped');
-		var liftref = _getLiftRef(lift);
-		var i = floorref;
-		var interval = setInterval(function() {
-			//$('tr[data-floor='+(i+1)+']').find('td[data-lift='+liftref+']').removeClass('moving');	
-		    $('tr[data-floor='+i+']').find('td[data-lift='+liftref+']').addClass('moving');	
-		    i--
-		    if(i<floorref) {
-		        clearInterval(interval);
-		        //alert("Code to be executed after completing loop");
-		        $('tr[data-floor='+floorref+']').find('td[data-lift='+liftref+']').removeClass('moving').addClass('stopped');	
-		    }
-		}, 1500);
-	}
-
-	var _getNearest = function(t){
+	var _callToFloor = function(t){
 		var currentref = _getFloor(t);
 		var first = $('.lift.stopped').eq(0);
 		var nearest = first;
@@ -53,20 +60,32 @@ var Escalator = (function($, undefined){
 		});
 		var floorref = _getFloor(nearest);
 		console.log('Nearest lift is on: ' + floorref + ' and cu');
-		if ( floorref < currentref) {
-			_moveUp(nearest, currentref);
-		} else {
-			_moveDown(nearest, currentref);
-		}
+		//_move(nearest, currentref);
+		
+		if ( floorref !== currentref) {
+			_move(nearest, currentref);
+		} 
 
+	}
+
+	var _selectFloor = function(t){
+		var sourceref = _getFloor(t);
+		var targetref = parseInt($(t).val(),10);
+		var thislift =  $('tr[data-floor='+sourceref+']').find('.lift.stopped').eq(0);
+		if ( sourceref !== targetref) {
+			_move(thislift, targetref);
+		} 
 	}
 
 	var _bindEvents = function(){
 		$('table').on('click', 'button', function(e){
 			e.preventDefault();
 			console.log('current floor: ' + _getFloor(this));
-			_getNearest(this);
+			_callToFloor(this);
+		}).on('change', 'select', function(){
+			_selectFloor(this);
 		})
+
 	}
 
 	var _init = function(){
