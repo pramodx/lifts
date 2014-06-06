f59a31636a53d0ee6d1f4adc6714f4b1ad2a0cb0
pramodx <pramodx@gmail.com>
2014-06-07 02:06:09 +1000

    db

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var Escalator = (function($, undefined){
-
+	var _dbUpdate = true;
 	var _getFloor = function(t){
 		return $(t).parents('tr').attr('data-floor');
 	}
@@ -40,11 +40,14 @@ var Escalator = (function($, undefined){
 		        clearInterval(interval);
 		        //alert("Code to be executed after completing loop");
 		       targetfloor.find('td[data-lift='+liftref+']').removeClass('moving').addClass('stopped');	
+		       if (_dbUpdate){
+		       	_dbdata.setData(liftref, targetfloorref);
+		       }
 		       targetfloor.attr('data-liftcount', parseInt(targetfloor.attr('data-liftcount'),10)+1);
 		        targetfloor.find('select').addClass('show');
 		        topStatus.find('p').removeClass('arrow-up arrow-down');
 		    }
-		}, 1500);
+		}, 1000);
 	}
 	
 
@@ -52,20 +55,62 @@ var Escalator = (function($, undefined){
 		var currentref = _getFloor(t);
 		var first = $('.lift.stopped').eq(0);
 		var nearest = first;
-		console.log(first);
+		
 		$('.lift.stopped').each(function(e){
 			if (Math.abs(_getFloor(this) - currentref) < Math.abs(_getFloor(nearest) - currentref )){
 				nearest = this;
 			}
 		});
 		var floorref = _getFloor(nearest);
-		console.log('Nearest lift is on: ' + floorref + ' and cu');
-		//_move(nearest, currentref);
 		
 		if ( floorref !== currentref) {
 			_move(nearest, currentref);
 		} 
 
+	}
+
+	var _rest = function (argument) {
+		var apiKey = 'SUNCexY8jmspBkpwUsua0ymB_vVipNNI',
+			_id = '5391cf44e4b0d4536359e35b',
+			url = 'https://api.mongolab.com/api/1/databases/pramodx/collections/elevators/?apiKey=' + apiKey;// + '&u=true&q={_id:' + _id + '}';
+		this.data
+		this.getData = function(){
+			
+			var _self = this;
+			$.ajax({
+				url: url,
+				dataType: 'json',
+				async: false,
+				type: 'get',
+				crossDomain: true,
+				success: function(data){
+					$(data).each( function(i){
+						$('tr[data-floor='+data[i].floor+']').find('button').click();
+					});
+				},
+				error: function(e){
+					console.log(e);
+				}
+			})
+			//return data;
+		}
+
+		this.setData = function(idx, floor){
+			
+			$.ajax({ 
+				url: url + '&u=true&q={_id:' + idx + '}',
+	  			data: JSON.stringify( { "$set" : { "floor" : floor } } ),
+	  			type: 'put',
+	  			contentType: "application/json",
+	  			crossDomain: true,
+	  			success: function(){
+	  				console.log('Data Entered');
+	  			},
+	  			error: function(e){
+	  				console.log(e);
+	  			}
+	  		});
+		}
 	}
 
 	var _selectFloor = function(t){
@@ -80,23 +125,34 @@ var Escalator = (function($, undefined){
 	var _bindEvents = function(){
 		$('table').on('click', 'button', function(e){
 			e.preventDefault();
-			console.log('current floor: ' + _getFloor(this));
 			_callToFloor(this);
 		}).on('change', 'select', function(){
 			_selectFloor(this);
 		})
 
+		$('.fetchFloors').on('click', function(){
+			_fetchfromdb();
+		})
+
 	}
 
+	var _dbdata = new _rest();
+
+	var _fetchfromdb = function(){
+		_dbUpdate = false;
+		var data = _dbdata.getData();
+		_dbUpdate = true;
+	}
+	
+
 	var _init = function(){
-
 		_bindEvents();
-
-
 	}
 
 	return {
-		init: _init
+		init: _init,
+		dbdata: _dbdata,
+		fetchfromdb: _fetchfromdb
 	}
 
 })(jQuery, undefined)
